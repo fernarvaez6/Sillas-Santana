859118f2c35c4d5d0434b1dcfc256634695742bb
CesarMiramontesQ <alexmiramontes15@gmail.com>
2018-09-14 14:37:07 -0600

    mm

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,11 @@
 const functions = require('firebase-functions');
-
+const nodemailer = require('nodemailer');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+exports.sendEmailConfirmation = functions.database.ref('/consultas').onWrite((change) => {
+  console.log( change )
+});
